1f94a3dbcfb03b2d61b689c0184fc844ae9b95c0
vova916 <volodumur086@gmail.com>
2026-05-26 20:29:49 +0300

    потужняк

--- reg_login/views.py
+++ reg_login/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from . models import Room, Booking, User, Category
 
-# Create your views here.
+
+def home(request):
+    rooms = Room.objects.all()
+    
+    
+    return render(request, 'index.html', {'title': 'Головна сторінка',
+                                          'rooms': rooms})
+
+
